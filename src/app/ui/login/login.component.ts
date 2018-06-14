fd02ae1bf1eb622b7b6f7f7d708cecff93a594d8
Magnus Trandokken <magnus@magson.no>
2018-06-14 15:09:26 +0200

    fuck u

--- src/app/ui/login/login.component.ts
+++ src/app/ui/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from './../../services/auth-service.service';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
   }
+  constructor() {
+    public auth: AuthServiceService,
+  }
 
+ 
+
+  signInWithGithub(){
+    const that = this;
+    this.auth.loginWithGithub();
+  }
 }
