abb492f625efc8ed1f002a3777fec8ed9d4feba5
sondresorbye0111 <sondre@magson.no>
2018-06-20 22:17:16 +0200

    fixing

--- src/app/ui/navbar/navbar.component.ts
+++ src/app/ui/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
+import { AuthServiceService } from './../../services/auth-service.service';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -8,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  profileUrl: string;
+  profileName: string;
+  profileEmail: string;
+
+  sub: Subscription;
+
+  constructor(public auth: AuthServiceService) {
+    this.sub = auth.user$.subscribe((user) => {
+      this.profileUrl = user.photoURL;
+      this.profileName = user.displayName;
+      this.profileEmail = user.email;
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
