9bc5db60c1ce5c3ebd496fb32145320915f873f5
Magnus <magnus@trandokken.com>
2018-07-22 22:21:08 +0200

    bugs from magnus with love

--- src/app/ui/team-settings/team-settings.component.ts
+++ src/app/ui/team-settings/team-settings.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from '../../../../node_modules/rxjs/Observable';
 import { Subscription } from '../../../../node_modules/rxjs';
+import swal from 'sweetalert2';
+import { AngularFireFunctions } from 'angularfire2/functions';
+
+
 
 @Component({
   selector: 'app-team-settings',
@@ -19,7 +23,9 @@ export class TeamSettingsComponent implements OnInit, OnDestroy {
   sub: Subscription;
 
   constructor(public route: ActivatedRoute, public boardsService: BoardsService,
-    public afs: AngularFirestore, public navbarService: NavbarService) {
+    public afs: AngularFirestore,
+    public navbarService: NavbarService,
+    public afFunctions: AngularFireFunctions) {
   }
 
   ngOnInit() {
@@ -34,4 +40,66 @@ export class TeamSettingsComponent implements OnInit, OnDestroy {
     this.navbarService.backBtn = false;
   }
 
+  promoteAdmin() {
+
+  }
+
+  deleteMember() {
+
+  }
+
+  addMember(teamId: string, teamName: string, input: string) {
+    const that = this;
+
+    const getUserByMail = this.afFunctions.httpsCallable('getUserByMail');
+    return getUserByMail({ mail: input }).toPromise()
+      .then(function (data) {
+        const uid = data.userData.uid;
+        const imageUrl = data.userData.photoURL;
+        const displayName = data.userData.displayName;
+
+        swal.queue([{
+          text: `Are you sure you want to add ${displayName} to ${teamName}?`,
+          imageUrl: imageUrl,
+          imageWidth: 100,
+          imageHeight: 100,
+          imageAlt: 'Scrumboard user profile',
+          confirmButtonText: 'Add',
+          showCancelButton: true,
+          reverseButtons: true,
+          showLoaderOnConfirm: true,
+          preConfirm: () => {
+            // add to team
+            const ref = that.afs.firestore.doc('teams/' + teamId);
+            that.afs.firestore.runTransaction(transaction => transaction.get(ref).then(doc => {
+              const members = doc.data().members;
+              members[uid] = 'mail';
+              return transaction.update(ref, { members });
+            }).then(() => {
+              swal({
+                title: `Success`,
+                type: 'success',
+                text: `You have successfully invited ${displayName} to your team!`,
+              });
+            })
+              .catch(err => {
+                swal({
+                  title: 'Error',
+                  text: err,
+                  type: 'error'
+                });
+              }));
+          }
+        }]);
+      }).catch(function (error) {
+        swal.insertQueueStep({
+          title: 'Could not find user',
+          text: error.message,
+          type: 'error',
+        });
+
+      });
+  }
+}]);
+  }
 }
