8aa1e82030dcd381339a37a99531d492ec7cbea2
Magnus <magnus@trandokken.com>
2018-07-27 18:50:13 +0200

    magnus

--- src/app/ui/team-settings/team-settings.component.ts
+++ src/app/ui/team-settings/team-settings.component.ts
@@ -19,6 +19,7 @@ import { AngularFireFunctions } from 'angularfire2/functions';
 export class TeamSettingsComponent implements OnInit, OnDestroy {
 
   team$: Observable<TeamsInterface>;
+  teamId: string;
 
   sub: Subscription;
 
@@ -29,8 +30,8 @@ export class TeamSettingsComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    const teamId = this.route.snapshot.paramMap.get('teamId');
-    this.team$ = this.afs.doc<TeamsInterface>('teams/' + teamId).valueChanges();
+    this.teamId = this.route.snapshot.paramMap.get('teamId');
+    this.team$ = this.afs.doc<TeamsInterface>('teams/' + this.teamId).valueChanges();
     this.navbarService.backBtn = true;
     this.sub = this.team$.subscribe(team => this.navbarService.title = team.name);
   }
@@ -48,58 +49,59 @@ export class TeamSettingsComponent implements OnInit, OnDestroy {
 
   }
 
-  addMember(teamId: string, teamName: string, input: string) {
+  addMember(input: string) {
+    console.log(input);
     const that = this;
+    this.team$.shareReplay(1).subscribe(team => {
+      console.log(team);
+      const getUserByMail = this.afFunctions.httpsCallable('getUserByMail');
+      return getUserByMail({ mail: input }).toPromise()
+        .then(function (data) {
+          const uid = data.userData.uid;
+          const imageUrl = data.userData.photoURL;
+          const displayName = data.userData.displayName;
 
-    const getUserByMail = this.afFunctions.httpsCallable('getUserByMail');
-    return getUserByMail({ mail: input }).toPromise()
-      .then(function (data) {
-        const uid = data.userData.uid;
-        const imageUrl = data.userData.photoURL;
-        const displayName = data.userData.displayName;
-
-        swal.queue([{
-          text: `Are you sure you want to add ${displayName} to ${teamName}?`,
-          imageUrl: imageUrl,
-          imageWidth: 100,
-          imageHeight: 100,
-          imageAlt: 'Scrumboard user profile',
-          confirmButtonText: 'Add',
-          showCancelButton: true,
-          reverseButtons: true,
-          showLoaderOnConfirm: true,
-          preConfirm: () => {
-            // add to team
-            const ref = that.afs.firestore.doc('teams/' + teamId);
-            that.afs.firestore.runTransaction(transaction => transaction.get(ref).then(doc => {
-              const members = doc.data().members;
-              members[uid] = 'mail';
-              return transaction.update(ref, { members });
-            }).then(() => {
-              swal({
-                title: `Success`,
-                type: 'success',
-                text: `You have successfully invited ${displayName} to your team!`,
-              });
-            })
-              .catch(err => {
+          swal.queue([{
+            text: `Are you sure you want to add ${displayName} to ${team.name}?`,
+            imageUrl: imageUrl,
+            imageWidth: 100,
+            imageHeight: 100,
+            imageAlt: 'Scrumboard user profile',
+            confirmButtonText: 'Add',
+            showCancelButton: true,
+            reverseButtons: true,
+            showLoaderOnConfirm: true,
+            preConfirm: () => {
+              // add to team
+              const ref = that.afs.firestore.doc('teams/' + this.teamId);
+              that.afs.firestore.runTransaction(transaction => transaction.get(ref).then(doc => {
+                const members = doc.data().members;
+                members[uid] = 'mail';
+                return transaction.update(ref, { members });
+              }).then(() => {
                 swal({
-                  title: 'Error',
-                  text: err,
-                  type: 'error'
+                  title: `Success`,
+                  type: 'success',
+                  text: `You have successfully invited ${displayName} to your team!`,
                 });
-              }));
-          }
-        }]);
-      }).catch(function (error) {
-        swal.insertQueueStep({
-          title: 'Could not find user',
-          text: error.message,
-          type: 'error',
-        });
+              })
+                .catch(err => {
+                  swal({
+                    title: 'Error',
+                    text: err,
+                    type: 'error'
+                  });
+                }));
+            }
+          }]);
+        }).catch(function (error) {
+          swal.insertQueueStep({
+            title: 'Could not find user',
+            text: error.message,
+            type: 'error',
+          });
 
-      });
-  }
-}]);
+        });
+    });
   }
 }
