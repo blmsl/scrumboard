9cec331a98a130b68f1b0434cb062e3fb8cb6f0a
Magnus Trandokken <magnus@magson.no>
2018-11-17 14:43:56 +0100

    Bug: Error when adding team member

--- angular/src/app/ui/team-settings/team-settings.component.ts
+++ angular/src/app/ui/team-settings/team-settings.component.ts
@@ -232,7 +232,7 @@ export class TeamSettingsComponent implements OnInit, OnDestroy {
               })
                 .catch(err => {
                   that.loading = false;
-                  console.log(err.code);
+                  console.log(err);
                   console.log(err.message);
                   swal({
                     title: 'Error',
